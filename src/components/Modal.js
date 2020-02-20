af4cf1e3502bc654d04743862c4afd2efa37e8e5
kbreslin <coding.kathrynbreslin@gmail.com>
2020-02-20 15:22:35 -0500

    rendering different content basedon items in shopping cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,40 @@ import React from "react";
 import { Modal, Button, Card } from "react-bootstrap";
 
 function CartModal(props) {
+    if (props.cart.length > 0) {
+        return (
+            <Modal show={props.show} onHide={props.close}>
+            <Modal.Header closeButton>
+                <Modal.Title>Your Cart</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {props.cart.map(item => {
+                    return (
+                        <Card style={{ width: '18rem' }} key={item.id}>
+                            <Card.Img variant="top" src={item.image} />
+                            <Card.Body>
+                                <Card.Title>{item.name}</Card.Title>
+                                <Card.Text>
+                                    {item.text}
+                                    Quantity: {item.quantity}
+                                </Card.Text>
+                                <Button variant="primary">Remove</Button>
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={props.close}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={props.close}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        )
+    }
     return (
         <>
             <Modal show={props.show} onHide={props.close}>
@@ -9,21 +43,7 @@ function CartModal(props) {
                     <Modal.Title>Your Cart</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {props.cart.map(item => {
-                        return (
-                            <Card style={{ width: '18rem' }} key={item.id}>
-                                <Card.Img variant="top" src={item.image} />
-                                <Card.Body>
-                                    <Card.Title>{item.name}</Card.Title>
-                                    <Card.Text>
-                                        {item.text}
-                                        Quantity: {item.quantity}
-                                    </Card.Text>
-                                    <Button variant="primary">Remove</Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })}
+                    No products in cart
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.close}>
