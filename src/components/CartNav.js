f3a76c34101ffe2e36435516c4a768c9a6a9f7aa
kbreslin <coding.kathrynbreslin@gmail.com>
2020-03-09 14:13:44 -0400

    updated shopping cart icon to show total amount in the cart

--- src/components/CartNav.js
+++ src/components/CartNav.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-// import SvgIcon from "@material-ui/core/SvgIcon";
 import "./CartNav.css";
+
+const StyledBadge = withStyles(theme => ({
+    badge: {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}))(Badge);
 
 function CartNav(props) {
     return (
         <Nav className="justify-content-end" activeKey="/">
             <Nav.Item className="item-1">
-                {/* <Nav.Link href="/home">Active</Nav.Link> */}
                 {props.searchBar}
             </Nav.Item>
-            <Nav.Item>
+            {/* <Nav.Item>
                 <Nav.Link eventKey="link-1">Total: {props.totalCartQuantity}</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={props.openClick}>
-                    <ShoppingCartIcon/>
-                </Nav.Link>
+            </Nav.Item> */}
+            <Nav.Item className="item-2">
+                {/* <Nav.Link onClick={props.openClick}>
+                    <ShoppingCartIcon />
+                </Nav.Link> */}
+                <IconButton>
+                    <StyledBadge badgeContent={parseInt(props.totalCartQuantity)} onClick={props.openClick} color="secondary">
+                        <ShoppingCartIcon/>
+                    </StyledBadge>
+                </IconButton>
             </Nav.Item>
         </Nav>
     )
