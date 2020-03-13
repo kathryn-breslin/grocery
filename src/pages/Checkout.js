0d395374ff36976d21421fd8793f0ca05cf1c06e
kbreslin <coding.kathrynbreslin@gmail.com>
2020-03-13 11:33:35 -0400

    update structure of checkout to include a card and grid system

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import "./Checkout.css";
 import { Link } from "react-router-dom";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Button from '@material-ui/core/Button';
+
 class Checkout extends Component {
     constructor() {
         super();
@@ -32,48 +40,73 @@ class Checkout extends Component {
     render() {
         if ((this.state.show) && (this.state.cart)) {
             return (
-                <div className="container-fluid">
+                <div className="container" style={{ width: '600px' }}>
                     <h4>Review Your Order</h4>
-                    <div className="container-fluid">
-                        {console.log("State in Checkout: " + JSON.stringify(this.state.cart))}
-                        {console.log("Props on the checkout page: " + JSON.stringify(this.props.history.location.state))}
-                        {this.state.cart.map(item => {
-                            return (
-                                <div className="row">
-                                    <div className="col-12 mt-3">
-                                        <div className="card" key={item.id} id={item.id}>
-                                            <div className="card-horizontal">
-                                                <div className="img-square-wrapper">
-                                                    <img className="" src={item.image} alt="Card image cap" />
-                                                </div>
-                                                <div className="card-body">
-                                                    <h4 className="card-title">{item.title}</h4>
-                                                    <p className="card-text">Quantity: {item.quantity}</p>
-                                                </div>
-                                            </div>
-                                            <div className="card-footer">
-                                                <small className="text-muted">Remove Item</small>
-                                                <button variant="warning" onClick={() => this.removeFromCart(item)}>Remove</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                        <Link to={{
-                            pathname: "/",
-                            state: this.state.cart
-                        }}>Continue Shopping</Link>
+                    {console.log("State in Checkout: " + JSON.stringify(this.state.cart))}
+                    {console.log("Props on the checkout page: " + JSON.stringify(this.props.history.location.state))}
+                    <Card style={{ minWidth: 275 }}>
+                        <CardContent>
+                            {this.state.cart.map(item => {
+                                return (
+                                    <Paper style={{ padding: 2, margin: 'auto', maxWidth: 500, marginTop: 20 }}>
+                                        <Grid container spacing={2}>
+                                            <Grid item>
+                                                <ButtonBase style={{ width: 128, height: 128 }}>
+                                                    <img src={item.image} style={{ margin: 'auto', display: 'block', maxWidth: '100%', maxHeight: '100%' }} />
+                                                </ButtonBase>
+                                            </Grid>
 
-                    </div>
+                                            <Grid item xs={12} sm container>
+                                                <Grid item xs container direction="column" spacing={2}>
+                                                    <Grid item xs>
+                                                        <Typography gutterBottom variant="subtitle1">
+                                                            {item.title}
+                                                        </Typography>
+                                                    </Grid>
+                                                    <Grid item>
+                                                        <Typography>
+                                                            <Button onClick={() => this.removeFromCart(item)}>Remove</Button>
+                                                        </Typography>
+                                                    </Grid>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography variant="subtitle1">Quantity: {item.quantity} </Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </Paper>
+                                )
+                            })}
+                            <Link to={{
+                                pathname: "/",
+                                state: this.state.cart
+                            }} style={{ float: 'right', marginTop: '40px', margin: '0 auto' }}>Continue Shopping</Link>
+                        </CardContent>
+                    </Card>
                 </div>
             )
         }
         else {
             return (
-                <div>
-                    <h1>Your cart is currently empty</h1>
-                    <Link to="/">Continue Shopping</Link>
+                <div className="container" style={{ width: '600px' }}>
+                    <h4>Review Your Order</h4>
+
+                    <Card style={{ minWidth: 275 }}>
+                        <CardContent>
+                            <Paper style={{ padding: 2, margin: 'auto', maxWidth: 500, marginTop: 20 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item>
+                                        <Typography variant="subtitle1">No Items in Cart</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+
+                            <Link to={{
+                                pathname: "/",
+                                state: this.state.cart
+                            }} style={{ float: 'right', marginTop: '40px', margin: '0 auto' }}>Continue Shopping</Link>
+                        </CardContent>
+                    </Card>
                 </div>
             )
         }
